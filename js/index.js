3c44246fcfe1ec8f495ec92953e848d93318e7da
RokerRy <rishabh1689.be20@chitkara.edu.in>
2022-02-20 03:34:34 +0530

    signup section design

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
     $("#signup-section").hide();
-    $("#signupbutton").click(function()
+    $("#signup-btn").click(function()
     {
         $("#login-section").fadeOut();
         setTimeout(function(){
             $("#signup-section").fadeIn();
+        },400);
+    });
+    $("#gbt-login").click(function()
+    {
+        $("#signup-section").fadeOut();
+        setTimeout(function(){
+            $("#login-section").fadeIn();
         },400);
     });
     var myModalEl = document.getElementById('modalform')
